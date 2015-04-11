854f655cf3c518079b4b60353e8828501626c500
Sunil Kalmadka <skalmadka@gmail.com>
2015-04-11 16:10:06 -0400

    First Commit

--- app.js
+++ app.js
@@ -57,9 +57,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(port, function(){
-  console.log('Listening on port %s', port);
-});
+app.set('port', (process.env.PORT || 5000));
 
 
 module.exports = app;
